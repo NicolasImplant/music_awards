d759e33884f516159bf8b17e6e8d15b71c4ad926
Nico <nicolasimplant@gmail.com>
2022-01-21 18:01:35 -0500

    End class 19, Status Code: Ok, Functions based views

--- music_awards/polls/urls.py
+++ music_awards/polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='Index'),
+    path('<int:question_id>', views.detail, name='Detail'),
+    path('<int:question_id>/results/', views.results, name='Results'),
+    path('<int:question_id>/votes/', views.votes, name='Votes'),
 ]
--- music_awards/polls/views.py
+++ music_awards/polls/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello word')
+    return HttpResponse('Estas en la página principal de music awards')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'Estas viendo la pregunta número {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta número {question_id}')
+
+
+def votes(request, question_id):
+    return HttpResponse(f'Estas votando a la pregunta número {question_id}')
