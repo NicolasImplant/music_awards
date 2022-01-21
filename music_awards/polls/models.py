4d82ebe9c2876bc4183e079cd7b0a97590a3af7c
Nico <nicolasimplant@gmail.com>
2022-01-21 13:26:56 -0500

    End class 13, Status code: Ok, __STR__ method

--- music_awards/polls/models.py
+++ music_awards/polls/models.py
@@ -1,9 +1,18 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     """Modelo de pregunta"""
     question_text  = models.CharField(max_length = 200)
     pub_date       = models.DateTimeField("Date published")
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)  
+
     
 
 class Choice(models.Model):
@@ -11,3 +20,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choise_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choise_text
