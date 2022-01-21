7a71a93558e0af9a044580501209733103c94a3b
Nico <nicolasimplant@gmail.com>
2022-01-21 12:58:34 -0500

    End class 11, Status code: Ok, Create models app

--- music_awards/polls/models.py
+++ music_awards/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    """Modelo de pregunta"""
+    question_text  = models.CharField(max_length = 200)
+    pub_date       = models.DateTimeField("Date published")
+    
+
+class Choices(models.Model):
+    """Modelo de opcion de respuesta"""
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
