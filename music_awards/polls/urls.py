8496a1492de11f77319cef4b50853273ec1abec4
Nico <nicolasimplant@gmail.com>
2022-01-23 10:43:10 -0500

    End class 23, Status code: Ok, Change Hard Coding

--- music_awards/polls/urls.py
+++ music_awards/polls/urls.py
@@ -2,9 +2,11 @@ from unicodedata import name
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='Index'),
-    path('<int:question_id>', views.detail, name='Detail'),
+    path('<int:question_id>/soyelmejor/', views.detail, name='Detail'),
     path('<int:question_id>/results/', views.results, name='Results'),
     path('<int:question_id>/votes/', views.votes, name='Votes'),
 ]
