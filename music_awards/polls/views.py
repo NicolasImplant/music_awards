fbc02f622c3f240dddb17807cf144218b995beca
Nico <nicolasimplant@gmail.com>
2022-01-24 17:56:11 -0500

    End class 26, status code: Ok, Results view created

--- music_awards/polls/views.py
+++ music_awards/polls/views.py
@@ -20,7 +20,12 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f'Estas viendo los resultados de la pregunta número {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html',{
+        'question': question
+        }
+    )
+    
 
 
 def votes(request, question_id):
